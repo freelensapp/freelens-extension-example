496e93cc001bfffe0ef086cf9d24be66e5d085fd
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2025-06-16 01:24:34 +0200

    Example preferences; fixes for K8s API (#57)

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,3 +1,8 @@
 import { Main } from "@freelensapp/extensions";
+import { ExamplePreferencesStore } from "../common/store";
 
-export default class LensExtensionExampleMain extends Main.LensExtension {}
+export default class ExampleMain extends Main.LensExtension {
+  async onActivate() {
+    await ExamplePreferencesStore.getInstanceOrCreate().loadExtension(this);
+  }
+}
--- src/renderer/k8s/example/example.ts
+++ src/renderer/k8s/example/example.ts
@@ -3,9 +3,6 @@ import { NamespacedObjectReference } from "./types";
 
 type KubeObjectMetadata = Renderer.K8sApi.KubeObjectMetadata;
 
-const KubeObject = Renderer.K8sApi.KubeObject;
-const KubeObjectStore = Renderer.K8sApi.KubeObjectStore;
-
 export interface ExampleSpec {
   title?: string;
   description?: string;
@@ -14,22 +11,16 @@ export interface ExampleSpec {
 
 export interface ExampleStatus {}
 
-export class Example extends KubeObject<KubeObjectMetadata, ExampleStatus, ExampleSpec> {
+export class Example extends Renderer.K8sApi.KubeObject<KubeObjectMetadata, ExampleStatus, ExampleSpec> {
   static readonly kind = "Example";
   static readonly namespaced = true;
   static readonly apiBase = "/apis/example.freelens.app/v1alpha1/examples";
 }
 
 export class ExampleApi extends Renderer.K8sApi.KubeApi<Example> {}
-export const exampleApi = new ExampleApi({ objectConstructor: Example });
-export class ExampleStore extends KubeObjectStore<Example> {
-  api = exampleApi;
-}
-export const exampleStore = new ExampleStore();
-Renderer.K8sApi.apiManager.registerStore(exampleStore);
+export class ExampleStore extends Renderer.K8sApi.KubeObjectStore<Example, ExampleApi> {}
 
 export const exampleObject = {
   kind: "Example",
   apiVersions: ["example.freelens.app/v1alpha1"],
-  api: exampleApi,
 };
